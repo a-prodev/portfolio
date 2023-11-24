1ff11ec2ee036b85a3a21bfc8eb48a6cc365cc08
Jose Brandao <josepedrosbr@gmail.com>
2023-11-24 08:52:29 -0300

    Some information replace

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -7,21 +7,21 @@ import { Card } from "../components/card";
 const socials = [
 	{
 		icon: <Twitter size={20} />,
-		href: "https://twitter.com/chronark_",
+		href: "https://twitter.com/_prodev",
 		label: "Twitter",
-		handle: "@chronark_",
+		handle: "@_prodev",
 	},
 	{
 		icon: <Mail size={20} />,
-		href: "mailto:dev@chronark.com",
+		href: "mailto:josepedrosbr@gmail.com",
 		label: "Email",
-		handle: "dev@chronark.com",
+		handle: "josepedrosbr@gmail.com",
 	},
 	{
 		icon: <Github size={20} />,
-		href: "https://github.com/chronark",
+		href: "https://github.com/a-prodev",
 		label: "Github",
-		handle: "chronark",
+		handle: "a-prodev",
 	},
 ];
 
