8be83d2e7f95d7ed5b182020ea7275a5ab175e2b
Jose Brandao <josepedrosbr@gmail.com>
2023-12-05 18:44:52 -0300

    Add AWS S3 client and configuration

---
+++ lib/s3-image.tsx
@@ -0,0 +1,26 @@
+import Image from 'next/image'
+import { s3 } from '@/lib/s3-client'
+
+
+export default function S3Image(
+  { s3Key, width, height, alt = '' }: { s3Key: string, width: number, height: number, alt?: string }
+) {
+  const params = {
+    Bucket: 'apps-development',
+    Key: s3Key,
+  };
+
+  const url = s3.getSignedUrl('getObject', params);
+
+  return (
+    <>
+      <Image
+        src={url}
+        width={width}
+        height={height}
+        alt={alt}
+      />
+    </>
+  );
+
+}
